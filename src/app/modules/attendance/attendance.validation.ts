5e20750a3a497e05cad1809a9fa16193d98c4599
mayesha6 <soumyrhmn234@gmail.com>
2026-08-25 09:55:58 +0600

    validation

--- src/app/modules/attendance/attendance.validation.ts
+++ src/app/modules/attendance/attendance.validation.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 const markAttendanceSchema = z.object({
-  body: z.object({
-    token: z.string().min(1, { message: "QR token is required" }),
-    bookingId: z.string().optional(),
-  }),
+
+  token: z.string().min(1, { message: "QR token is required" }),
+  bookingId: z.string().optional(),
+
 });
 
 export const AttendanceValidations = {
