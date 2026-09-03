4c473b16483ce6c0dea99374e498a0a6ecd349be
mayesha6 <soumyrhmn234@gmail.com>
2026-09-03 11:43:36 +0600

    checkout

--- src/app/modules/gymdesk/gymdesk.routes.ts
+++ src/app/modules/gymdesk/gymdesk.routes.ts
@@ -10,7 +10,7 @@ router.post("/webhook", GymdeskController.handleWebhook);
 
 // Protected Checkout URL generator route (supports both /checkout-url and /checkout-url/:planId)
 router.get(
-  "/checkout-url/:planId?",
+  "/checkout-url/:planId",
   checkAuth(Role.MEMBER, Role.USER, Role.COACH, Role.ADMIN, Role.SUPER_ADMIN),
   GymdeskController.getCheckoutUrl
 );
